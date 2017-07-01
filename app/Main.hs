1c6b099425515d425d2d9574c999b5c0f6c7bccd
Gabriel Gonzalez <gabriel@awakenetworks.com>
2017-07-01 05:53:00 -0700

    Make `--key` optional
    This updates `gen-aws-sigkey` to be able to take the secret access key from
    `~/.aws/credentials` instead of the command line
    
    Really, the right thing to do would be to use a library like `amazonka` or `aws`
    to obtain the credentials so that we could take advantage of multiple ways to
    read credentials (like environment variables).
    
    However, `aws-configuration-tools` flat out did not work and using `amazonka`
    requires more extensive changes to this library since you cannot extract the
    secret access key from the opaque `Auth` type

--- app/Main.hs
+++ app/Main.hs
@@ -1,29 +1,64 @@
 {-# LANGUAGE MultiWayIf        #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards   #-}
+{-# LANGUAGE QuasiQuotes       #-}
 
 module Main where
 
+import qualified Control.Lens
 import           Data.Aeson
 import qualified Data.ByteString.Lazy.Char8 as C8L
+import qualified Data.Ini
 import           Data.Maybe
+import qualified Data.Text
 import qualified Data.Text.IO               as Text.IO
 import           Data.Thyme.Clock
+import qualified NeatInterpolation
 import           Options.Generic
+import qualified Turtle
+import           Turtle                     ((</>))
 
 import qualified Gen.AWS.SigKey             as SigKey
 
 main :: IO ()
 main = do
   SigKey.Options{..} <- unwrapRecord "Generate a AWS V4 scoped signing key"
-  now <- getCurrentTime
+
+  now  <- getCurrentTime
+
+  secretAccessKey <- case key of
+    Just secretAccessKey -> return secretAccessKey
+    Nothing              -> do
+      home <- Turtle.home
+      let credentialsFile = home </> ".aws" </> "credentials"
+      exists <- Turtle.testfile credentialsFile
+      if not exists
+        then fail (Data.Text.unpack [NeatInterpolation.text|
+Error: Cannot obtain secret access key
+
+If you do not provide the `--key` flag then there you must supply a credentials
+file at `~/.aws/credentials` instead
+|])
+        else do
+          text <- Turtle.readTextFile credentialsFile
+          let fold =
+                  Control.Lens._Right
+                . Control.Lens.to Data.Ini.unIni
+                . Control.Lens.ix "default"
+                . Control.Lens.ix "aws_secret_access_key"
+          case Control.Lens.preview fold (Data.Ini.parseIni text) of
+            Just secretAccessKey -> return secretAccessKey
+            Nothing -> fail (Data.Text.unpack [NeatInterpolation.text|
+Error: Could not decode the credentials file located at `~/.aws/credentials`
+|])
+
   let ssk@SigKey.ScopedSigningKey{..} =
         SigKey.makeKey
           now
           (fromMaybe "us-east-1" region)
           (fromMaybe "s3" service)
           (fromMaybe "aws4_request" protocol)
-          key
+          secretAccessKey
 
   if | sigonly   -> Text.IO.putStrLn signing_key
      | otherwise -> C8L.putStrLn $ encode ssk
